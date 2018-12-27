99379a568796c0fafa479742c9ebe8c43e84e251
christophpickl <christoph.pickl@gmail.com>
2018-12-27 19:43:48 +0100

    add basic detect

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
 object Versions {
     val kpotpourri = "2.2"
@@ -11,6 +12,7 @@ plugins {
     idea
     id("com.github.johnrengelman.shadow") version "4.0.3"
     id("com.github.ben-manes.versions") version "0.20.0"
+    id("io.gitlab.arturbosch.detekt").version("1.0.0-RC12")
 }
 
 val artifactName = "DerBauer"
@@ -80,3 +82,33 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
     }
     checkForGradleUpdate = true
 }
+/*
+first run: $ ./gradlew detektGenerateConfig
+
+detekt {
+    toolVersion = "[version]"                             // Version of the Detekt CLI that will be used. When unspecified the latest detekt version found will be used. Override to stay on the same version.
+    input = files(                                        // The directories where detekt looks for input files. Defaults to `files("src/main/java", "src/main/kotlin")`.
+        "src/main/kotlin",
+        "gensrc/main/kotlin"
+    )
+    parallel = false                                      // Runs detekt in parallel. Can lead to speedups in larger projects. `false` by default.
+    config = files("path/to/config.yml")                  // Define the detekt configuration(s) you want to use. Defaults to the default detekt configuration.
+    baseline = file("path/to/baseline.xml")               // Specifying a baseline file. All findings stored in this file in subsequent runs of detekt.
+    filters = ''                                          // Regular expression of paths that should be excluded separated by `;`.
+    disableDefaultRuleSets = false                        // Disables all default detekt rulesets and will only run detekt with custom rules defined in `plugins`. `false` by default.
+    plugins = "other/optional/ruleset.jar"                // Additional jar file containing custom detekt rules.
+    debug = false                                         // Adds debug output during task execution. `false` by default.
+    reports {
+        xml {
+            enabled = true                                // Enable/Disable XML report (default: true)
+            destination = file("build/reports/detekt.xml")  // Path where XML report will be stored (default: `build/reports/detekt/detekt.xml`)
+        }
+        html {
+            enabled = true                                // Enable/Disable HTML report (default: true)
+            destination = file("build/reports/detekt.html") // Path where HTML report will be stored (default: `build/reports/detekt/detekt.html`)
+        }
+    }
+}
+
+
+ */
